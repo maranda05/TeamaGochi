185a1eb6cf0f952b31aebaa24406268abc1e9c99
maranda05 <arandamrt19@gmail.com>
2023-09-18 16:04:16 -0300

    cambio prueba

--- TeamaGochi_Home/views.py
+++ TeamaGochi_Home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "TeamaGochi_Home/index.html")
+    return render(request, "../../../TeamaGochi_Home/index.html")
