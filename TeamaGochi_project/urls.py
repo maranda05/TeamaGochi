7a4448796880ddf37e849afaede509442d58a999
maranda05 <arandamrt19@gmail.com>
2023-09-18 17:34:16 -0300

    cambio 2 18-08

--- TeamaGochi_project/urls.py
+++ TeamaGochi_project/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('TeamaGochi_home.urls')),
-    path('', views.index),
+    path('', include('TeamaGochi_Home.urls')),
+    path('', views.index, name='index'),
 ]
