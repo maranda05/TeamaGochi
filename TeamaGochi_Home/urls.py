c7e5880f086286485b6d1d4b7e4888dd7458c96e
maranda05 <arandamrt19@gmail.com>
2023-10-01 14:44:09 -0300

    cambio 1-10

--- TeamaGochi_Home/urls.py
+++ TeamaGochi_Home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('term-cond/',views.term_and_condition),
+    path('creators/',views.creators)
 ]
--- TeamaGochi_Home/views.py
+++ TeamaGochi_Home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "TeamaGochi_Home/index.html")
+
+def term_and_condition(request):
+    return render(request, "TeamaGochi_Home/term_and_condition.html")
+
+def creators(request):
+    return render(request, "TeamaGochi_Home/Creators.html")
