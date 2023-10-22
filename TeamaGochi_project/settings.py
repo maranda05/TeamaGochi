62a5f13b065c024d23734218db5c234e6c7e8e41
bojeda <bojeda@usm.cl>
2023-10-22 13:06:00 -0300

    migración bbdd

--- TeamaGochi_project/settings.py
+++ TeamaGochi_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'TeamaGochi_Home.apps.TeamaGochi_HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
