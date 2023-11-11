70c747cfb978ef1cc00579b77a6838178d28896e
maranda05 <arandamrt19@gmail.com>
2023-11-11 20:44:51 -0300

    kjghj

--- TeamaGochi_Home/urls.py
+++ TeamaGochi_Home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio),
     path('index/', views.index),
+    path('creators/',views.creators),
     path('index/creators/',views.creators),
     path('term-cond/',views.term_and_condition),
     path('index/logout/', views.exit),
--- TeamaGochi_project/settings.py
+++ TeamaGochi_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     #'TeamaGochi_Home.apps.TeamaGochi_HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'TeamaGochi_Home'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
