43a82d78084857c6d719e09b5bc70fc747ebe6ab
Trenxzzz <felipemvega04@gmail.com>
2023-11-26 14:36:51 -0300

    Cambios Finales mueje

--- TeamaGochi_Home/models.py
+++ TeamaGochi_Home/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class AuthGroup(models.Model):
@@ -133,3 +134,12 @@ class information(models.Model):
     class Meta:
         managed = False
         db_table = 'information'
+
+class AnimalGuardado(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    Animal = models.ForeignKey(information, on_delete=models.CASCADE)
+    fecha_guardado = models.DateTimeField(auto_now_add=True)
+
+class intermed (models.Model):
+    id_user = models.IntegerField(User)
+    id_animal = models.IntegerField()
